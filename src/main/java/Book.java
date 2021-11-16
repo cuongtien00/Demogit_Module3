60c7a388ceecfcef16ce36cb2116aca059b8bcc8
cuongtien00 <cuonghiep000@gmail.com>
2021-11-16 16:26:12 +0700

    add book

---
+++ src/main/java/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    private int id;
+    private String book;
+
+    public Book() {
+    }
+
+    public Book(int id, String book) {
+        this.id = id;
+        this.book = book;
+    }
+}
